b4a39c3c805752121ce7d91bc8b18523f3047001
Chengkai Zhang <zchengkai826@gmail.com>
2019-05-14 21:16:38 +0800

    receive mouse data!

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -19,13 +19,15 @@ void init_mouse_cursor8(unsigned char *mouse) {
 void main() {
   struct GATE_DESCRIPTOR idt[256];
   struct BOOTINFO *binfo = (struct BOOTINFO *)ADR_BOOTINFO;
-  unsigned char keybuf[32];
+  unsigned char keybuf[32], mousebuf[128];
 
   io_cli();
   init_idt(idt);
   init_pic();
   set_gatedesc(idt+0x21, (unsigned)asm_inthandler21, 1 << 3, AR_INTGATE32);
   set_gatedesc(idt+0x2c, (unsigned)asm_inthandler2c, 1 << 3, AR_INTGATE32);
+  fifo8_init(&keyfifo, 32, keybuf);
+  fifo8_init(&mousefifo, 128, mousebuf);
   init_keyboard();
   enable_mouse();
   io_out8(PIC0_IMR, 0xf9);
@@ -39,19 +41,26 @@ void main() {
   init_mouse_cursor8(mcursor);
   putfont8(binfo->vram, binfo->scrnx, 100, 125, COL8_FFFFFF, mcursor);
 
-  fifo8_init(&keyfifo, 32, keybuf);
-
+  unsigned char i;
+  char s[8];
   for(;;) {
     io_cli();
-    if(fifo8_status(&keyfifo) == 0) {
+    if(fifo8_status(&keyfifo) + fifo8_status(&mousefifo) == 0) {
       io_stihlt();
     } else {
-      unsigned char i = fifo8_get(&keyfifo);
-      char s[8];
-      io_sti();
-      u2s(s, i);
-      boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 64, 32);
-      putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+      if(fifo8_status(&keyfifo) != 0) {
+        i = fifo8_get(&keyfifo);
+        io_sti();
+        u2s(s, i);
+        boxfill8(binfo->vram, binfo->scrnx, COL8_008484, 0, 0, 64, 32);
+        putfonts8_asc(binfo->vram, binfo->scrnx, 0, 0, COL8_FFFFFF, s);
+      } else if(fifo8_status(&mousefifo) != 0) {
+        i = fifo8_get(&mousefifo);
+        io_sti();
+        u2s(s, i);
+        boxfill8(binfo->vram, binfo->scrnx, COL8_848400, 128, 0, 192, 32);
+        putfonts8_asc(binfo->vram, binfo->scrnx, 128, 0, COL8_FFFFFF, s);
+      }
     }
   }
 }
