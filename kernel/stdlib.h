67680828cd33babdc5dd3bcb1e05542428d64879
Chengkai Zhang <zchengkai826@gmail.com>
2019-05-21 13:14:33 +0800

    add fifo32

--- kernel/stdlib.h
+++ kernel/stdlib.h
@@ -2,6 +2,15 @@
 #define _STDLIB_H
 
 /* fifo.c */
+struct FIFO32 {
+  int *buf;
+  int p, q, size, free, flags;
+};
+void fifo32_init(struct FIFO32 *fifo, int size, int *buf);
+int fifo32_put(struct FIFO32 *fifo, int data);
+int fifo32_get(struct FIFO32 *fifo);
+int fifo32_status(struct FIFO32 *fifo);
+
 struct FIFO8 {
   unsigned char *buf;
   int p, q, size, free, flags;
